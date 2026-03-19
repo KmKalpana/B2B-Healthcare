7efe1c22c8020a1614a1632b12ee2c2006236a26
Kalpana Kathait <kalpanakathait11@gmail.com>
2026-03-20 00:27:50 +0530

    added env file

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,22 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB-rx45gfAQDDyp3VLvoJhlkwkVdxLtfbg",
-  authDomain: "b2b-healthcare-246e4.firebaseapp.com",
-  projectId: "b2b-healthcare-246e4",
-  storageBucket: "b2b-healthcare-246e4.firebasestorage.app",
-  messagingSenderId: "730347137183",
-  appId: "1:730347137183:web:c316aa9f31602878bb9aaa",
-  measurementId: "G-KDH285GKQB"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
 export const db = getFirestore(app);
-
 
 export { app, analytics, auth };
